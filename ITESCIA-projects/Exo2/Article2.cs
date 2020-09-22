e17262fb66f8cefd48746c592130ad86952c5437
Valentin Stockman <valentin@smartnsoft.com>
2020-09-22 13:42:19 +0200

    Add Exo 2

---
+++ ITESCIA-projects/Exo2/Article2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ITESCIA_projects.Exo2
+{
+    public class Article2
+    {
+        protected string Nom;
+        protected double Prix;
+        protected int Quantite;
+
+        public Article2(string Nom, double Prix, int Quantite) { this.Nom = Nom; this.Prix = Prix; this.Quantite = Quantite; }
+        public void Afficher()
+        {
+            Console.WriteLine("Nom de l'article : " + Nom + ", prix de l'article : " + Prix + " , quantité : " + Quantite);
+        }
+
+        public void Ajouter(int nombre)
+        {
+            Quantite = Quantite + nombre;
+            Console.WriteLine("Nouvelle quantité de " + Nom + " : " + Quantite);
+        }
+
+        public void Retirer(int nombre)
+        {
+            Quantite = Quantite - nombre;
+            Console.WriteLine("Nouvelle quantité de " + Nom + " : " + Quantite);
+        }
+
+    }
+}
--- ITESCIA-projects/Program.cs
+++ ITESCIA-projects/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ITESCIA_projects.Exo1;
+using ITESCIA_projects.Exo2;
 
 namespace ITESCIA_projects
 {
@@ -7,12 +8,30 @@ namespace ITESCIA_projects
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Exo 1 :");
              var article = new Article("video", 29.00);
              var disque = new Disque("Les Enfoirés", "Disque", 19.99);
             var  video = new Video(23.54, "Video", 39.99);
             article.Acheter();
             disque.Ecouter();
             video.Afficher();
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Exo 2 :");
+            var article2 = new Article2("Manga", 19.99, 2);
+            var article3 = new Article2("La reine des neiges", 29.99, 1);
+            article2.Afficher();
+            article3.Afficher();
+            article2.Retirer(1);
+            article3.Ajouter(2);
+            Console.WriteLine("Tapez le nom de l'article :");
+            string title = Console.ReadLine().ToString();
+            Console.WriteLine("Tapez le prix de l'article :");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tapez la quantité de l'article :");
+            int quantity = Convert.ToInt16(Console.ReadLine());
+            var articleUser = new Article2(title, price, quantity);
+            articleUser.Afficher();
+            Console.WriteLine("------------------------------------------------------------");
         }
     }
 }

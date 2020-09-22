b85156b36e02961a322775afe47d0e47b900caa8
Valentin Stockman <valentin@smartnsoft.com>
2020-09-22 13:16:47 +0200

    Add getters and setters for Article's properties

--- ITESCIA-projects/Exo1/Article.cs
+++ ITESCIA-projects/Exo1/Article.cs
@@ -3,10 +3,20 @@ namespace ITESCIA_projects.Exo1
 {
     public class Article
     {
-        protected string Designation { get; set; }
-        protected double Prix { get; set; }
+        protected string Designation;
+        public string MaDesignation
+        {
+            get { return Designation; }
+            set { Designation = value; }
+        }
+        protected double Prix;
+        public double MonPrix
+        {
+            get { return Prix; }
+            set { Prix = value; }
+        }
 
-        public Article(string Designation, double Prix) { this.Designation = Designation; this.Prix = Prix; }
+        public Article(string Designation, double Prix) { this.MaDesignation = Designation; this.MonPrix = Prix; }
         public void Acheter()
         {
             Console.WriteLine("Prix de l'article : " + Prix + ", designation : " + Designation);

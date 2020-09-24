b809713107c716ed605d7165913d4b0f40da209f
Valentin Stockman <valentin@smartnsoft.com>
2020-09-24 11:41:10 +0200

    Add Exo 7

---
+++ ITESCIA-projects/Exo6&7/ListeChainee.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITESCIA_projects.Exo6
+{
+    class Liste
+    {
+        private Element _Debut;
+        private readonly Element[] arr = new Element[100];
+
+        public void RemplirTableau()
+        {
+            Element debut = _Debut;
+            if (debut != null)
+            {
+                int i = 0;
+                arr[i] = debut;
+                while (debut.Suivant != null)
+                {
+                    debut = debut.Suivant;
+                    i++;
+                    arr[i] = debut;
+                }
+            }
+        }
+
+        public Element this[int i]
+        {
+            get => arr[i];
+            set => arr[i] = value;
+        }
+
+        public int NbElements { get; private set; }
+
+        public Liste()
+        {
+            _Debut = null;
+            NbElements = 0;
+        }
+        public void InsererDebut(object premier_objet)
+        {
+            Element newDebut = new Element(premier_objet)
+            {
+                Suivant = _Debut
+            };
+            _Debut = newDebut;
+            NbElements++;
+            RemplirTableau();
+        }
+
+        public void InsererFin(object dernier_objet)
+        {
+            Element newFin = new Element(dernier_objet);
+            if (_Debut == null)
+            {
+                _Debut = newFin;
+                NbElements++;
+                return;
+            }
+            Element dernierElement = RecupereDernierElement();
+            dernierElement.Suivant = newFin;
+            NbElements++;
+            RemplirTableau();
+        }
+
+        public Element RecupereDernierElement()
+        {
+            Element surf = _Debut;
+            while (surf.Suivant != null)
+            {
+                surf = surf.Suivant;
+            }
+            return surf;
+        }
+
+        public void Lister()
+        {
+            Element debut = _Debut;
+            if (debut != null)
+            {
+                string cumul = debut.Objet.ToString();
+                while (debut.Suivant != null)
+                {
+                    debut = debut.Suivant;
+                    cumul += "," + debut.Objet.ToString();
+                }
+                Console.WriteLine(cumul);
+            }
+            else
+            {
+                Console.WriteLine("La liste est vide");
+            }
+        }
+
+        public void Vider()
+        {
+            _Debut = null;
+            NbElements = 0;
+            RemplirTableau();
+        }
+
+    }
+
+    class Element
+    {
+        public object Objet { get; set; }
+        public Element Suivant { get; set; }
+
+        public Element(object objet)
+        {
+            Objet = objet;
+            Suivant = null;
+        }
+    }
+}
--- ITESCIA-projects/Program.cs
+++ ITESCIA-projects/Program.cs
@@ -92,6 +92,55 @@ namespace ITESCIA_projects
             {
                 personnesArray[i].Afficher();
             }
+
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Exo 7 :");
+            Employe employe1 = new Employe("Valentin", "Stockman", 25, 3000);
+            Employe employe2 = new Employe("Mickey", "Mouse", 35, 2000);
+            Employe employe3 = new Employe("Employe", "Employe", 20, 1600); ;
+            Employe employe4 = new Employe("Employe2", "Employe2", 30, 2400);
+            Employe employe5 = new Employe("Gogeta", "Super Sayan", 45, 3700);
+            Chef chef1 = new Chef("Chef 1", "nom Chef 1", 33, 3000, "Marketing");
+            Chef chef2 = new Chef("Chef 2", "nom Chef 2", 30, 3200, "Informatique");
+            Directeur directeur = new Directeur("Jean", "Charles", 38, 4500, "R&D", "ITESCIA");
+
+            Console.WriteLine("Insertion des éléments à la liste");
+
+            Liste personnes = new Liste();
+            personnes.InsererDebut(directeur);
+            personnes.InsererDebut(chef2);
+            personnes.InsererDebut(chef1);
+            personnes.InsererDebut(employe5);
+            personnes.InsererDebut(employe4);
+            personnes.InsererDebut(employe3);
+            personnes.InsererDebut(employe2);
+            personnes.InsererDebut(employe1);
+
+            Console.WriteLine("Nombre d'éléments : {0}", personnes.NbElements);
+            personnes.Lister();
+
+            Console.WriteLine("Vidage de la liste");
+            personnes.Vider();
+
+            Console.WriteLine("Nombre d'éléments : {0}", personnes.NbElements);
+            personnes.Lister();
+
+            Console.WriteLine("Avec insertion de fin");
+            personnes.InsererFin(employe1);
+            personnes.InsererFin(employe2);
+            personnes.InsererFin(employe3);
+            personnes.InsererFin(employe4);
+            personnes.InsererFin(employe5);
+            personnes.InsererFin(chef1);
+            personnes.InsererFin(chef2);
+            personnes.InsererFin(directeur);
+
+            Console.WriteLine("Nombre d'éléments : {0}", personnes.NbElements);
+            Console.WriteLine("Avec indexeur");
+            for (int i = 0; i < personnes.NbElements; i++)
+            {
+                Console.WriteLine(personnes[i].Objet.ToString());
+            }
         }
     }
 }
